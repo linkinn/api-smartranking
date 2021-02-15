608b7e259443b4f6164ddf47fa8d1766daae0017
Fillipi Nascimento <fillipi-nascimento@hotmail.com>
2021-02-15 17:47:12 -0300

    feat: create config mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { JogadoresModule } from './jogadores/jogadores.module';
 
 @Module({
-  imports: [JogadoresModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/smartranking', {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    }),
+    JogadoresModule,
+  ],
   controllers: [],
   providers: [],
 })
